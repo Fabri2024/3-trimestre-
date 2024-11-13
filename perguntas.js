b296bd0f1b830b2a5231e4df1e78ce9fa9e98d4c
Fabri2024 <165902182+Fabri2024@users.noreply.github.com>
2024-11-13 07:59:27 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,57 @@
+criaCartao(
+    'futebol',
+    'quantos campeonatos estaduais o Londrina tem?',
+    '5'
+)
+
+criaCartao(
+    'futebol',
+    'Quem venceu a copa do Brasil de 2021?',
+    'atlético mineiro'
+)
+
+criaCartao(
+    'futebol',
+    'Quem venceu a libertadores de 2019? Qual foi o placar?',
+    'Flamengo, 2x1'
+)
+
+criaCartao(
+    'politica',
+    'Quem foi o presidente do Brasil entre os anos 2018 á 2022? Qualera o seu número?',
+    'Jair Messias Bolsonaro e o número era 17)'
+)
+criaCartao(
+    'futebol',
+    'Qual seleção tem mais copas do mundo?',
+    'Brasil'
+)
+
+criaCartao(
+    'números',
+    'Qual é o número representado por LXIV?',
+    '64'
+)
+
+criaCartao(
+    'geografia',
+    'Em qual continente fica localizado o pais da Letônia?',
+    'Europa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
